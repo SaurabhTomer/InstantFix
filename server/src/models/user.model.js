eed6e853f1611d70b497d2c4616df52a490f9940
Saurabh Tomer <tomersaurabh91@gmail.com>
2026-02-05 13:56:03 +0530

    Add user model with address schema for MongoDB

---
+++ server/src/models/user.model.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+
+const addressSchema = new mongoose.Schema({
+ 
+  street: String,
+  city: String,
+  state: String,
+  pincode: String,
+  location: {
+    lat: Number,
+    lng: Number
+  },
+  isDefault: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+
+    phone: {
+      type: String,
+      required: true,
+      unique: true
+    },
+
+    password: {
+      type: String,
+      required: true
+    },
+
+    role: {
+      type: String,
+      enum: ["USER", "ELECTRICIAN", "ADMIN"],
+      default: "USER"
+    },
+
+    address: [addressSchema],
+  },
+
+  { timestamps: true }
+  
+);
+
+export default mongoose.model("User", userSchema);
